616d23807292499eca279a325ffc6a0afeb04ab3
Vin Xue <vinxue@outlook.com>
2020-04-28 16:02:55 +0800

    DumpMem: Add a application to dump memory to file.

---
+++ Application/DumpMem/DumpMem.h
@@ -0,0 +1,28 @@
+/** @file
+  function definitions for internal to application functions.
+
+  Copyright (c) 2020, Gavin Xue. All rights reserved.<BR>
+  This program and the accompanying materials
+  are licensed and made available under the terms and conditions of the BSD License
+  which accompanies this distribution.  The full text of the license may be found at
+  http://opensource.org/licenses/bsd-license.php
+
+  THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+  WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+**/
+
+#ifndef _DUMP_LOG_H_
+#define _DUMP_LOG_H_
+
+#include <PiDxe.h>
+#include <Uefi.h>
+#include <Library/UefiLib.h>
+#include <Library/DebugLib.h>
+#include <Library/ShellLib.h>
+#include <Library/BaseLib.h>
+#include <Library/UefiBootServicesTableLib.h>
+#include <Library/BaseMemoryLib.h>
+#include <Library/MemoryAllocationLib.h>
+#include <Library/BaseMemoryLib.h>
+
+#endif
