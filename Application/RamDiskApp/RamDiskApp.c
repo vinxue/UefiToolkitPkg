6952e3f883834214569ed7cb21b297c6c545f73d
Vin Xue <vinxue@outlook.com>
2023-02-16 13:23:25 +0800

    RamDiskApp: Set memory type of RAM disk to reserved

--- Application/RamDiskApp/RamDiskApp.c
+++ Application/RamDiskApp/RamDiskApp.c
@@ -1,7 +1,7 @@
 /** @file
   A UEFI tool for RAM disk demo.
 
-  Copyright (c) 2021, Gavin Xue. All rights reserved.<BR>
+  Copyright (c) 2021 - 2023, Gavin Xue. All rights reserved.<BR>
   This program and the accompanying materials
   are licensed and made available under the terms and conditions of the BSD License
   which accompanies this distribution.  The full text of the license may be found at
@@ -58,7 +58,7 @@ ReadFileFromDisk (
   }
 
   Status = gBS->AllocatePool (
-                  EfiBootServicesData,
+                  EfiReservedMemoryType,
                   (UINTN) FileSize,
                   (VOID**) &FileBuffer
                   );
